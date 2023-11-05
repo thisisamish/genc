576794f63a5a6713c8c7c1f2eda3ea483632d8cb
Amish Verma <amishhverma@gmail.com>
2023-11-05 21:24:43 +0530

    homepage ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+import { Button } from '@radix-ui/themes';
+
 export default function Home() {
-	return <main>GenC</main>;
+	return (
+		<main className="flex items-center justify-center h-screen">
+			<div className="flex flex-col">
+				<h1 className="text-center text-5xl font-bold mb-4">GenC</h1>
+				<p className="mb-20">
+					Create and verify certificate IDs with ease
+				</p>
+				<div className="flex gap-2">
+					<Button className="flex-1">
+						<Link href="/generate">Generate IDs</Link>
+					</Button>
+					<Button className="flex-1">
+						<Link href="/verify">Verify IDs</Link>
+					</Button>
+				</div>
+			</div>
+		</main>
+	);
 }
